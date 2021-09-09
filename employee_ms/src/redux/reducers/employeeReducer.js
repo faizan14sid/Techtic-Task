f2e8c55e5b9c67ee666b51af0db358448f62c315
faizan14sid <faizan14sid@gmail.com>
2021-09-10 01:22:31 +0530

    delete leaves

--- employee_ms/src/redux/reducers/employeeReducer.js
+++ employee_ms/src/redux/reducers/employeeReducer.js
@@ -38,25 +38,7 @@ const employeeReducer = (state = "", action) => {
             console.log(action.payload, "delete")
             return {
                 ...state,
-                items: [...state.items.filter((emp) => emp.id === action.payload.id ? {
-                    ...emp.leaves.filter((leave) => {
-                        return (
-                            <>
-                                {console.log(leave)}
-                            </>
-                        )
-                    })
-                } : emp)]
-                // items: [...state.items.filter((emp) => emp.id === action.payload.id ? {
-                //     ...emp.leaves.map((leave) => {
-                //         return {
-                //             [
-                //                 leave === action.payload.subList.toString() ? { ...emp.leaves.filter(leave) } : 
-                //             ]
-                //             //  ...emp.leaves.pop()
-                //         }
-                //     })
-                // } : emp)]
+                items: state.items.filter((emp) => emp.id === action.payload.id ? { ...emp.leaves.shift() } : emp)
 
             }
         default:
